7c51831c3219c608331db382dcdec07dafc78e37
zJacoz <jaco.net91@gmail.com>
2026-01-13 21:47:31 -0300

    controller updated

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,52 +1,50 @@
 <?php
-require_once __DIR__ . "/../models/model.php";
+require_once __DIR__ . "/../models/Aluno.php";
 require_once __DIR__ . "/../helpers/response.php";
-
 
 class AlunoController
 {
-    private $model;
+    private Aluno $model;
 
     public function __construct()
     {
-        $this->model = new AlunoModel();
+        $this->model = new Aluno();
     }
 
     public function index()
     {
         $alunos = $this->model->listar();
-        return response("success", "Lista de alunos", $alunos);
+
+        if (empty($alunos)) {
+            return response("error", "Nenhum aluno encontrado");
+        }
+
+        return response("success", "Alunos encontrados", $alunos);
     }
 
     public function store($nome, $nota)
     {
-        if (empty($nota) || !is_numeric($nota) || $nota < 0 || $nota > 10) {
-            return response("error", "Dados inválidos.");
+        if (empty($nome) || !is_numeric($nota) || $nota < 0 || $nota > 10) {
+            return response("error", "Dados inválidos");
         }
 
         $this->model->criar($nome, $nota);
-        return response("success", "Aluno criado.");
+        return response("success", "Aluno criado com sucesso");
     }
 
     public function update($nome, $nota)
     {
-        if (empty($nota) || !is_numeric($nota) || $nota < 0 || $nota > 10) {
-            return response("error", "Dados inválidos.");
+        if (empty($nome) || !is_numeric($nota)) {
+            return response("error", "Dados inválidos");
         }
 
-        if(!$this->model->atualizar($nome, $nota)) {
-            return response("error", "Aluno não encontrado.");
-        }
-
-        return response("success","Aluno atualizado.");
+        $this->model->atualizar($nome, $nota);
+        return response("success", "Aluno atualizado");
     }
 
-    public function destroy($nome) {
-        if(!$this->model->remover($nome)) {
-            return response("error", "Aluno não encontrado.");
-        }
-
-        return response("success","Aluno removido.");
+    public function destroy($nome)
+    {
+        $this->model->remover($nome);
+        return response("success", "Aluno removido");
     }
 }
-?>
