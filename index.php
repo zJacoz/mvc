899dd9f61d260cd92b1bff3b3dd5e89b25278c6c
zJacoz <jaco.net91@gmail.com>
2026-01-13 21:47:55 -0300

    index updated

--- index.php
+++ index.php
@@ -1,53 +1,47 @@
 <?php
-require_once "views/view.php";
-require_once "controllers/controller.php";
-require_once "helpers/response.php";
-
-function responses($status, $message, $data = null)
-{
-    return [
-        "status" => $status,
-        "message" => $message,
-        "data" => $data
-    ];
-}
+require_once "controllers/AlunoController.php";
 
 $controller = new AlunoController();
 
-echo "1 - GET\n";
-echo "2 - POST\n";
-echo "3 - PUT\n";
-echo "4 - DELETE\n";
-echo "5 - Sair\n";
+while (true) {
+    echo "1 - GET alunos\n";
+    echo "2 - POST aluno\n";
+    echo "3 - PUT aluno\n";
+    echo "4 - DELETE aluno\n";
+    echo "5 - Sair\n";
 
-$op = trim(fgets(STDIN));
+    $op = trim(fgets(STDIN));
 
-switch ($op) {
-    case 1:
-        render($controller->index());
-        break;
-    case 2:
-        echo "Nome: ";
-        $nome = trim(fgets(STDIN));
+    if ($op == 5) break;
 
-        echo "Nota: ";
-        $nota = trim(fgets(STDIN));
+    switch ($op) {
+        case 1:
+            echo json_encode($controller->index(), JSON_PRETTY_PRINT) . "\n";
+            break;
 
-        render($controller->store($nome, $nota));
-        break;
-    case 3:
-        echo "Nome: ";
-        $nome = trim(fgets(STDIN));
+        case 2:
+            echo "Nome: ";
+            $nome = trim(fgets(STDIN));
+            echo "Nota: ";
+            $nota = trim(fgets(STDIN));
 
-        echo "Nova nota: ";
-        $nota = trim(fgets(STDIN));
+            echo json_encode($controller->store($nome, $nota), JSON_PRETTY_PRINT) . "\n";
+            break;
 
-        render($controller->update($nome, $nota));
-        break;
-    case 4:
-        echo "Nome: ";
-        $nome = trim(fgets(STDIN));
-        render($controller->destroy($nome));
-        break;
+        case 3:
+            echo "Nome: ";
+            $nome = trim(fgets(STDIN));
+            echo "Nova nota: ";
+            $nota = trim(fgets(STDIN));
+
+            echo json_encode($controller->update($nome, $nota), JSON_PRETTY_PRINT) . "\n";
+            break;
+
+        case 4:
+            echo "Nome: ";
+            $nome = trim(fgets(STDIN));
+
+            echo json_encode($controller->destroy($nome), JSON_PRETTY_PRINT) . "\n";
+            break;
+    }
 }
-?>
