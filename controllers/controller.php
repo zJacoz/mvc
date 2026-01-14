b2447a508a9d6611916fc441a5e0d927fd52d89a
zJacoz <jaco.net91@gmail.com>
2026-01-13 21:16:02 -0300

    controller finished

---
+++ controllers/controller.php
@@ -0,0 +1,50 @@
+<?php
+require_once __DIR__ . "/../models/model.php";
+
+class AlunoController
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new AlunoModel();
+    }
+
+    public function index()
+    {
+        $alunos = $this->model->listar();
+        return response("success", "Lista de alunos", $alunos);
+    }
+
+    public function store($nome, $nota)
+    {
+        if (empty($nota) || !is_numeric($nota) || $nota < 0 || $nota > 10) {
+            return response("error", "Dados inválidos.");
+        }
+
+        $this->model->criar($nome, $nota);
+        return response("success", "Aluno criado.");
+    }
+
+    public function update($nome, $nota)
+    {
+        if (empty($nota) || !is_numeric($nota) || $nota < 0 || $nota > 10) {
+            return response("error", "Dados inválidos.");
+        }
+
+        if(!$this->model->atualizar($nome, $nota)) {
+            return response("error", "Aluno não encontrado.");
+        }
+
+        return response("success","Aluno atualizado.");
+    }
+
+    public function destroy($nome) {
+        if(!$this->model->remover($nome)) {
+            return response("error", "Aluno não encontrado.");
+        }
+
+        return response("success","Aluno removido.");
+    }
+}
+?>
